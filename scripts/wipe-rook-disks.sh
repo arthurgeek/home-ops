d9233359fc17d8ae67dce943d702d6b0b599e230
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2025-05-24 21:55:29 +0200

    chore: import old config: base services: rook, snapshot-controller and volsync

---
+++ scripts/wipe-rook-disks.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+source "$(dirname "${0}")/lib/common.sh"
+
+export LOG_LEVEL="debug"
+export ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+function main() {
+    # Copied from https://github.com/onedr0p/home-ops/blob/62df4717c653412b3f0b61534887579f3a9ef55c/scripts/bootstrap-cluster.sh
+    log debug "Wiping Rook disks"
+
+    # Skip disk wipe if Rook is detected running in the cluster
+    # NOTE: Is there a better way to detect Rook / OSDs?
+    if kubectl --namespace rook-ceph get kustomization rook-ceph &>/dev/null; then
+        log warn "Rook is detected running in the cluster, skipping disk wipe"
+        return
+    fi
+
+    if ! nodes=$(talosctl config info --output json 2>/dev/null | jq --exit-status --raw-output '.endpoints | join(" ")') || [[ -z "${nodes}" ]]; then
+        log error "No Talos nodes found"
+    fi
+
+    log debug "Talos nodes discovered" "nodes=${nodes}"
+
+    # Wipe disks on each node that match the ROOK_DISK environment variable
+    for node in ${nodes}; do
+        if ! disks=$(talosctl --nodes "${node}" get disk --output json 2>/dev/null |
+            jq --exit-status --raw-output --slurp '. | map(select(.spec.model == env.ROOK_DISK) | .metadata.id) | join(" ")') || [[ -z "${nodes}" ]]; then
+            log error "No disks found" "node=${node}" "model=${ROOK_DISK}"
+        fi
+
+        log debug "Talos node and disk discovered" "node=${node}" "disks=${disks}"
+
+        # Wipe each disk on the node
+        for disk in ${disks}; do
+            if talosctl --nodes "${node}" wipe disk "${disk}" &>/dev/null; then
+                log info "Disk wiped" "node=${node}" "disk=${disk}"
+            else
+                log error "Failed to wipe disk" "node=${node}" "disk=${disk}"
+            fi
+        done
+    done
+}
+
+main "$@"
